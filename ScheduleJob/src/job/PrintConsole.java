d9bb287195386c3ca780dbb6597192d27e4654c9
Ajuta <40140778+Ajuta@users.noreply.github.com>
2020-10-18 11:18:00 +0530

    Update PrintConsole.java

--- ScheduleJob/src/job/PrintConsole.java
+++ ScheduleJob/src/job/PrintConsole.java
@@ -4,7 +4,7 @@ public class PrintConsole {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Check if this is printed");
+		System.out.println("Check if this is printed pls");
 
 	}
 
